a15522a8f719d741d8ef1601660f9f30a9173f07
Haedam Hong <goeka1337@gmail.com>
2026-01-27 02:46:24 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 72.72 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,26 +1,40 @@
+// [구현 아이디어 1]
+// - 동명이인이 있을 수 있으므로 Set 사용불가.
+// 1. Map<String, Integer> 사용해서 participant를 저장
+// 2. completion을 돌면서 해당 이름들을 Map에서 찾아 Integer(해당이름 사람 수)를 -1함.
+// 3. 마지막에 Map에서 Integer != 0인 키값(이름)을 리턴함.
+
 import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Map<String, Integer> countMap = new HashMap<>();
-
-        // 참가자 이름별 등장 횟수 세기
-        for (String name : participant) {
-            countMap.put(name, countMap.getOrDefault(name, 0) + 1);
+        Map<String, Integer> nameCountMap = new HashMap<>();
+        
+        // 1. Map<String, Integer> 사용해서 participant를 저장
+        for(String p : participant){
+            if(nameCountMap.containsKey(p)){
+                int count = nameCountMap.get(p);
+                nameCountMap.replace(p, ++count);
+            }
+            else {
+                nameCountMap.put(p, 1);
+            }
         }
-
-        // 완주자 이름별 등장 횟수 차감
-        for (String name : completion) {
-            countMap.put(name, countMap.get(name) - 1);
+        // 2. completion을 돌면서 해당 이름들을 Map에서 찾아 value값 --
+        for(String cn : completion){
+            int count = nameCountMap.get(cn);
+            nameCountMap.replace(cn, --count);
         }
-
-        // 남은 값이 1인 사람 -> 완주 못한 사람
-        for (Map.Entry<String, Integer> entry : countMap.entrySet()) {
-            if (entry.getValue() != 0) {
-                return entry.getKey();
+        
+        // 3. Map에서 Integer != 0인 키값(이름)을 리턴
+        String notCompletedParticipant = "";
+        for(Map.Entry<String, Integer> entry : nameCountMap.entrySet()){
+            if(entry.getValue() != 0){
+                notCompletedParticipant = entry.getKey();
+                break;
             }
         }
         
-        return "";
+        return notCompletedParticipant;
     }
 }
